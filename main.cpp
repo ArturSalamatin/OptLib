c353af24e9fb66eacdda14e2be4d5190cd739995
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2024-03-27 16:09:19 +0300

    usage of some classes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <array>
+
+#include "src/AuxMethods/Point.h"
+#include "src/AuxMethods/Simplex.h"
 
 /// @brief program entry point
 /// @return code of execution result
 int main()
 {
+    OptLib::Point<3> P1{};
+    OptLib::Point<3> P2{};
+
+    auto it = P1.begin();
+
+    auto result{P1+P2};
+
+    std::cout << P1[0] << std::endl;
+    std::cout << P1 << std::endl;
+
     return 0;
 }
