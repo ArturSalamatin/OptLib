1432aae1231df57f9c1ae7fbfd2d8fb4535fe786
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2024-03-27 16:07:48 +0300

    move Point definition to a separate file

---
+++ src/AuxMethods/Point.h
@@ -0,0 +1,42 @@
+#ifndef POINT_H
+#define POINT_H
+
+#include <iostream>
+
+namespace OptLib
+{
+	/// @brief Container for scalar function arguments. Static,
+	/// compile-time allocation is assumed.
+	/// @tparam dim Dimensionality of space
+	template <size_t dim>
+	using RawPoint = std::array<double, dim>;
+
+	template <size_t dim>
+	class Point : private RawPoint<dim>
+	{
+	public:
+		using RawPoint<dim>::operator[];
+		//	using RawPoint<dim>::RawPoint;
+		using RawPoint<dim>::size;
+		using RawPoint<dim>::begin;
+		using RawPoint<dim>::end;
+		using RawPoint<dim>::iterator;
+		using RawPoint<dim>::reverse_iterator;
+		using RawPoint<dim>::value_type;
+	};
+	
+	template<size_t dim, typename Stream>
+	Stream& operator<<(Stream& o, const Point<dim>& p)
+	{
+		static_assert(dim > 0);
+		o << "{" << p[0];
+		for(size_t i = 1; i < dim; ++i)
+		{
+			o << ", " << p[i];
+		}
+		o << "}\n";
+		return o;
+	}
+} // OptLib
+
+#endif
