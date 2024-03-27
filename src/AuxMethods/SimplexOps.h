dc812293c101f1d4ae8a6f8198d861d884ddb7e6
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2024-03-27 14:12:22 +0300

    definitions of Points added

---
+++ src/AuxMethods/SimplexOps.h
@@ -0,0 +1,90 @@
+#pragma once
+#include <functional>
+
+namespace OptLib
+{
+	/// <summary>
+	/// An alias for std::array. It is required for the ease of RawPoint construction.
+	/// </summary>
+	template<size_t dim>
+	using Point = std::array<double, dim>;
+	//template<>
+	//using Point<1> = double;
+
+	namespace SimplexOps
+	{
+		namespace UnaryOps
+		{
+			template <class Scalar>
+			struct plus 
+			{
+				plus(Scalar value) : scalar{ value } {}
+
+				template<class T>
+				T operator() (const T& lhs)
+				{
+					return lhs + scalar;
+				}
+
+			protected:
+				Scalar scalar;
+			};
+
+			template <class Scalar>
+			struct minus
+			{
+				minus(Scalar value) : scalar{ value } {}
+
+				template<class T>
+				T operator() (const T& lhs)
+				{
+					return lhs - scalar;
+				}
+
+			protected:
+				Scalar scalar;
+			};
+			
+			template <class Scalar>
+			struct mult
+			{
+				mult(Scalar value) : scalar{ value } {}
+
+				template<class T>
+				T operator() (const T& lhs)
+				{
+					return lhs * scalar;
+				}
+
+			protected:
+				Scalar scalar;
+			};
+
+			struct sqrt
+			{
+				template<class T>
+				T operator() (const T& lhs)
+				{
+					return std::sqrt(lhs);
+				}
+			};
+
+		}
+		
+		namespace BinaryOps
+		{
+			template <class T = void>
+			using plus = std::plus<T>;
+
+			template <class T = void>
+			using minus = std::minus<T>;
+
+			template <class T = void>
+			using multiplies = std::multiplies<T>;
+
+
+
+		}
+	}
+}
+
