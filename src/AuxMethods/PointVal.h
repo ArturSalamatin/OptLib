166038d9deb48164fd90638ab1a6f639adc6adaa
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2024-03-28 21:09:57 +0300

    PointVal operators moved to a separate file

--- src/AuxMethods/PointVal.h
+++ src/AuxMethods/PointVal.h
@@ -1,15 +1,14 @@
 #ifndef POINTVAL_H
 #define POINTVAL_H
 
-#include <iostream>
-
 #include "Point/Point.h"
 #include "Point/PointOperators.h"
 
 namespace OptLib
 {
     /// <summary>
-    /// @brief Point with associated Value. Essentially a {Point, Val}-pair with a comparator to sort Points
+    /// @brief Aggregates Point with associated Value. 
+    /// Essentially a {Point, Val}-pair with a comparator to sort Points
     /// </summary>
     template <size_t dim>
     struct PointVal
@@ -27,83 +26,6 @@ namespace OptLib
         {
         }
     };
-
-    template <size_t dim>
-    bool operator<(const PointVal<dim> &lhs, const PointVal<dim> &rhs)
-    {
-        return lhs.Val < rhs.Val;
-    }
-
-    template <size_t dim>
-    PointVal<dim> operator+(const PointVal<dim> &arr1, const PointVal<dim> &arr2)
-    {
-        return PointVal<dim>{arr1.P + arr2.P, arr1.Val + arr2.Val};
-    }
-
-    /// elementwise subtraction of vector - vector
-    template <size_t dim>
-    PointVal<dim> operator-(const PointVal<dim> &arr1, const PointVal<dim> &arr2)
-    {
-        return PointVal<dim>{arr1.P - arr2.P, arr1.Val - arr2.Val};
-    }
-    /// elementwise multiplication of vector * vector
-    template <size_t dim>
-    PointVal<dim> operator*(const PointVal<dim> &arr1, const PointVal<dim> &arr2)
-    {
-        return PointVal<dim>{arr1.P * arr2.P, arr1.Val * arr2.Val};
-    }
-    template <size_t dim>
-    PointVal<dim> operator*(const PointVal<dim>& p, double val)
-    {
-        return {p.P * val, p.Val * val};
-    }
-    /// elementwise division of vector / vector
-    template <size_t dim>
-    PointVal<dim> operator/(const PointVal<dim> &arr1, const PointVal<dim> &arr2)
-    {
-        return {arr1.P / arr2.P, arr1.Val / arr2.Val};
-    }
-    /// elementwise division vector / scalar
-    template <size_t dim>
-    PointVal<dim> operator/(PointVal<dim> arr, double val)
-    {
-        return PointVal<dim>{arr.P / val, arr.Val / val};
-    }
-
-    ///  elementwise sqrt
-    template <size_t dim>
-    PointVal<dim> sqrt(const PointVal<dim> &p)
-    {
-        return {sqrt<dim>(p.P), std::sqrt(p.Val)};
-    }
-    ///  elementwise abs
-    template <size_t dim>
-    PointVal<dim> abs(const PointVal<dim> &p)
-    {
-        return {abs<dim>(p.P), std::abs(p.Val)};
-    }
-
-    template <size_t dim, typename Stream>
-    Stream &operator<<(Stream &o, const PointVal<dim> &r)
-    {
-        return o << "{ " << r.P << ", " << r.Val << " }";
-    }
-
-    /// <summary>
-    /// coefficient of variation, mean/variance
-    /// </summary>
-    /// <typeparam name="point"></typeparam>
-    /// <param name="avg"></param>
-    /// <param name="disp"></param>
-    /// <returns></returns>
-    template <typename point>
-    std::pair<point, point> VarCoef(const point &avg, const point &disp)
-    { // requires sqrt(vector), abs(vector), vector/vector
-        auto disp0 = sqrt(disp);
-        return std::pair{disp0 / abs(avg), disp0};
-    }
-
-   
 }
 
 #endif
