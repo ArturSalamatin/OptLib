46018902e789c6e57be468e974c6dd5c21fc3940
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2024-04-01 22:38:01 +0300

    States files introduced

---
+++ src/States/State.h
@@ -0,0 +1,124 @@
+#pragma once
+#include "stdafx.h"
+
+namespace OptLib
+{
+	namespace ConcreteState
+	{
+		/// <summary>
+		/// Direct methods require sorted simplex points with respect to f(x)
+		/// </summary>
+		template<size_t dim>
+		using StateDirect =  StateInterface::IStateSimplex<dim, SimplexValSort<dim>>;
+
+		/// <summary>
+		/// Simplexes for direct methods on segments (in 1D) must not be sorted with respect to f(x). Must be sorted with respect to x == x[0]
+		/// </summary>
+		class StateSegment : public StateInterface::IStateSimplex<1, SimplexValNoSort<1>>
+		{
+		public:
+			StateSegment(const StateSegment&) = default;
+			StateSegment(SetOfPoints<2, Point<1>>&& State, FuncInterface::IFunc<1>* f)
+				:
+				StateInterface::IStateSimplex<1, SimplexValNoSort<1>>(std::move(OrderPointsInSegment(State)), f)
+			{}
+
+		protected:
+			SetOfPoints<2, Point<1>> OrderPointsInSegment(SetOfPoints<2, Point<1>>& setOfPoints)
+			{
+				if (setOfPoints[0][0] > setOfPoints[1][0])
+					std::swap(setOfPoints[0], setOfPoints[1]);
+				return setOfPoints;
+			}
+		};
+
+		/// <summary>
+		/// First and second order methods require only a point rather than entire simplex for optimization
+		/// </summary>
+		template<size_t dim>
+		class StatePoint : public StateInterface::IState<dim>
+		{
+		protected:
+			PointVal<dim> dx{};
+		public:
+			bool IsConverged(double abs_tol, double rel_tol) const override
+			{
+				auto& std = dx;
+				auto var{ abs<dim>(dx / Guess()) };
+				for (int i = 0; i < dim; i++)
+				{
+					bool f = (((std[i]) < abs_tol) || (var[i] < rel_tol)) && (((std.Val) < abs_tol) || (var.Val < rel_tol));
+					if (!f) return false;
+				}
+				return true;
+			}
+
+			virtual void UpdateState(const PointVal<dim>& v)
+			{
+				dx = abs<dim>(v - Guess());
+				ItsGuess = v;
+			}
+
+		};
+
+		/// <summary>
+		/// Stochastic methods require multiple initial points
+		/// </summary>
+		template<size_t dim>
+		class StateStochastic : public StateInterface::IState<dim>
+		{
+		protected:
+			double(*Temperature) (double, int);
+			int iteration;
+			double endTemperature;
+			double h;
+		public:
+			double temperature;
+			PointVal<dim> NextRandomState()
+			{
+				PointVal<dim> x = Guess();
+				for (int i = 0; i < dim; i++) {
+					double r = ((double)rand() / RAND_MAX);
+					x.P[i] = 2.0 * r * h + x.P[i] - h;
+				}
+				return x;
+			}
+			PointVal<dim> bestGuess;
+			StateStochastic(Point<dim>&& State, FuncInterface::IFunc<dim>* f, double initialTemperature, double (*TemperatureFunction) (double, int), double step, double temperature_end) :
+				//StateInterface::IState<dim>(std::move(State), f), 
+				temperature{ initialTemperature }, Temperature{ TemperatureFunction }, h{ step }, endTemperature{ temperature_end },iteration{ 0 } 
+			{
+				ItsGuess = FuncInterface::CreateFromPoint(std::move(State), f);
+				bestGuess = ItsGuess;
+			}
+			bool IsConverged(double endTemperature, double) const override
+			{
+				return temperature > endTemperature ? false : true;
+			}
+			void ChangeGuess(PointVal<dim> currentGuess) {
+				ItsGuess = currentGuess;
+			}
+			void UpdateState()
+			{
+				iteration++;
+				temperature = Temperature(temperature, iteration);
+				PointVal<dim> currentGuess = NextRandomState(); //{ FuncInterface::CreateFromPoint(NextRandomState(Guess().P, h), f) };
+				//UpdateState();
+				double dp = exp((Guess().Val - currentGuess.Val) / temperature);
+				if (dp > 1)
+				{
+					ChangeGuess(currentGuess);
+				}
+				else
+				{
+					double p = (double)rand() / RAND_MAX;
+					if (dp > p)
+					{
+						ChangeGuess(currentGuess);
+					}
+				}
+				if (currentGuess < bestGuess) bestGuess = currentGuess;
+			}
+		};
+	} // ConcreteState
+} // OptLib
---
+++ src/States/StateInterface.h
@@ -0,0 +1,83 @@
+#pragma once
+#include "stdafx.h"
+
+namespace OptLib
+{
+	namespace StateInterface
+	{
+		/// <summary>
+		/// defines abstract basic functions, which are common for every type of state
+		/// </summary>
+		template<size_t dim>
+		class IState
+		{
+		protected:
+			PointVal<dim> ItsGuess;
+		public:
+			// concrete implementation depends on the order of optimization method
+			virtual bool IsConverged(double abs_tol, double rel_tol) const  = 0;
+			const PointVal<dim>& Guess() const { return ItsGuess; };
+		};
+
+		/// <summary>
+		/// State for methods of optimization in dim-dimensional space based on simplexes
+		/// </summary>
+		template<size_t dim, typename simplex>
+		class IStateSimplex : public StateInterface::IState<dim>
+		{
+		public: // overriden from predecessor
+			bool IsConverged(double abs_tol, double rel_tol) const override
+			{// is average and relative tolerance met?
+				auto [avg, disp] = GuessDomain().Dispersion();
+				auto [var,std] = VarCoef<PointVal<dim>>(avg, disp) ;
+
+				for (int i = 0; i < dim; i++)
+				{
+					bool f = (((std[i]) < abs_tol) || (var[i] < rel_tol)) && (((std.Val) < abs_tol) || (var.Val < rel_tol));
+					if (!f) return false;
+				}
+				return true;
+			}
+		protected:
+			simplex ItsGuessDomain; // the field is unique for direct optimization methods
+			std::array<double, dim + 1> FuncVals(const SetOfPoints<dim + 1, Point<dim>>& State, const FuncInterface::IFunc<dim>* f) 
+			{
+				return (*f)(State);
+			}
+			void UpdateDomain(SetOfPoints<dim + 1, Point<dim>>&& State, std::array<double, dim + 1>&& funcVals)
+			{
+				SetDomain(
+					simplex{ 
+						simplex::make_field(
+							std::move(State), 
+							std::move(funcVals)
+						) 
+					}
+				);
+			}
+		public:
+			IStateSimplex() {}
+			/*IStateSimplex(const SetOfPoints<dim + 1, Point<dim>>& State, FuncInterface::IFunc<dim>* f)
+			{
+				auto s{ State };
+				UpdateDomain(std::move(s), f);
+			}*/
+			IStateSimplex(SetOfPoints<dim + 1, Point<dim>>&& State, FuncInterface::IFunc<dim>* f)
+			{
+				UpdateDomain(std::move(State), f);
+			}
+			const simplex& GuessDomain() const { return ItsGuessDomain; } // unique for direct optimization methods
+			
+			void UpdateDomain(SetOfPoints<dim + 1, Point<dim>>&& State, const FuncInterface::IFunc<dim>* f)
+			{
+				UpdateDomain(std::move(State), std::move(FuncVals(State, f)));
+			}
+			virtual void SetDomain(SetOfPoints<dim + 1, PointVal<dim>>&& newDomain)
+			{
+				ItsGuessDomain = simplex{ std::move(newDomain) };
+
+				ItsGuess = GuessDomain().Mean();
+			}
+		};
+	} // StateInterface
+} // OptLib
