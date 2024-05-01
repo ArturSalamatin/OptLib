397fa7bd1d6317c0f55d278f156d6d17a013494b
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2024-05-01 13:21:08 +0300

    minor refactoring

--- src/Functions/Function.h
+++ src/Functions/Function.h
@@ -10,89 +10,9 @@ namespace OptLib
 {
 	namespace ConcreteFunc
 	{
-		/// <summary>
-		/// Templated paraboloid in dim-D space with gradient and hessian
-		/// </summary>
-		template <size_t dim>
-		class Paraboloid : public FuncInterface::IFuncWithHess<dim>
-		{
-		protected:
-			Hess<dim> hessian, coef_matrix;
-
-		public:
-			Paraboloid(const Hess<dim>& coefs) : hessian{coefs}, coef_matrix{coefs}
-			{
-				// transform coefficientts to hessian symmetric matrix
-				// make the hessian matrix symmetric
-				for (size_t i = 0; i < dim; ++i)
-				{
-					hessian[i][i] *= 2;
-					for (size_t j = i + 1; j < dim; ++j)
-					{
-						double temp = (hessian[i][j] + hessian[j][i]);
-						hessian[i][j] = temp;
-						hessian[j][i] = temp;
-					}
-				}
-				// make the coefficient matrix symmetric
-				for (size_t i = 0; i < dim; ++i)
-					for (size_t j = i + 1; j < dim; ++j)
-					{
-						double temp = (coef_matrix[i][j] + coef_matrix[j][i]) / 2.0;
-						coef_matrix[i][j] = temp;
-						coef_matrix[j][i] = temp;
-					}
-#ifdef DEBUG_LIB
-				std::cout << "Paraboloid in " << dim << "D space with Hessian "
-						  << hessian << " has been instantiated\n";
-				std::cout << "Its coefficient matrix A for xAx is " << CoefMatrix() << '\n';
-#endif // DEBUG_LIB
-			}
-
-			auto operator()(const Point<dim> &x) const -> typename Point<dim>::value_type override
-			{
-				Point<dim>::value_type result{0.0};
-				for (size_t i = 0; i < dim; ++i)
-				{
-					result += hessian[i][i] / 2.0 * x[i] * x[i];
-					for (size_t j = i + 1; j < dim; ++j)
-						result += hessian[i][j] * x[i] * x[j];
-				}
-				return result;
-			}
-
-			auto grad(const Point<dim> &x) const -> Grad<dim> override
-			{
-				Grad<dim> result{};
-				for (size_t i = 0; i < dim; ++i)
-					result[i] = 2 * dot_product(x, CoefMatrixRow(i));
-				return result;
-			}
-
-			auto hess(const Point<dim> &x) const -> Hess<dim> override
-			{
-				return hessian;
-			}
-
-		protected:
-			const SetOfPoints<dim, Point<dim>> &CoefMatrix() const
-			{
-				return coef_matrix;
-			}
-			const Point<dim> &CoefMatrixRow(size_t i) const
-			{
-				return CoefMatrix()[i];
-			}
-		};
-
-		/// <summary>
-		/// Paraboloid in 2D space, without hessian
-		/// </summary>
-		using Paraboloid2D = Paraboloid<2>;
-
-		/// <summary>
-		/// Parabola in 1D without hessian. mostly a test function...
-		/// </summary>
+		/**
+		 * @brief Parabola in 1D without hessian. mostly a test function...
+		*/
 		class Function : public FuncInterface::IFuncWithGrad<1>
 		{
 		public:
@@ -115,9 +35,10 @@ namespace OptLib
 			}
 		};
 
-		/// <summary>
-		/// Adding a hessian to a parabola
-		/// </summary>
+		/**
+		 * @brief Adding a hessian to a parabola
+		 * 
+		 */
 		class FunctionWithHess : public Function, public FuncInterface::IHess<1>
 		{
 		public:
@@ -170,7 +91,7 @@ namespace OptLib
 				return res;
 			}
 		};
-	} // ConcreteFuncs
+	} // ConcreteFunc
 } // OptLib
 
 #endif
