ace3dc749ab114e71a060deb722f48665fcc312e
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-04-03 15:20:15 +0300

    Wrapper class added

---
+++ dll_src/Wrapper.h
@@ -0,0 +1,20 @@
+#ifndef WRAPPER_H
+#define WRAPPER_H
+
+#include "Manager.h"
+#include "Operator.h"
+
+class Wrapper{
+public:
+  Wrapper();
+  ~Wrapper();
+
+  void useOperator();
+  void useManager();  
+
+private:
+  Operator m_operator;
+  Manager m_manager;
+};
+
+#endif // WRAPPER_H
